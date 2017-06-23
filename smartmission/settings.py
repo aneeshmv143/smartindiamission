04ac12f7124de74508c0227cc3381275e32201ed
vishnu <vishnu@smartindia.net.in>
2017-06-23 14:32:51 +0530

    Database changed to mysql

--- smartmission/settings.py
+++ smartmission/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'smartmission.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smartindiamission',
+        'USER': 'root',
+        'PASSWORD': 'root123',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
