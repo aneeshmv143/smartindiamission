84b1a96a67bd019bdc6d3ba401642d756a4d36f5
vishnu <vishnu@smartindia.net.in>
2017-10-26 13:49:25 +0530

    Fixed bugs in server settings

--- smartmission/settings.py
+++ smartmission/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'smartmission.context_procesor.get_category',
+                'smartmission.context_processors.get_category',
             ],
         },
     },
